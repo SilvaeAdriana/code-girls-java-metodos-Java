3f70011e6afcc0884503b6a29f6841c52401a194
A Driana <75712203+SilvaeAdriana@users.noreply.github.com>
2022-07-22 20:37:34 -0300

    Exercicio 2

--- Main.java
+++ Main.java
@@ -4,17 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Retornos
-        System.out.println("Exercício retornos");
-
-        double areaQuadrado = Quadrilatero.area(3);
-        System.out.println("Área do quadrado:" + areaQuadrado);
-
-        double areaRetangulo = Quadrilatero.area(5,5);
-        System.out.println("Área do retângulo:" + areaRetangulo);
-
-        double areaTrapezio = Quadrilatero.area(7,8,9);
-        System.out.println("Área do trapézio:" + areaTrapezio);
+        // Quadrilátero
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
 
     }
 }
--- Quadrilatero.java
+++ Quadrilatero.java
@@ -1,33 +1,28 @@
 package one.digitalinnovation.basecamp;
 
 /**
- * Classe de exemplo para o exercício da Aula 3 de Métodos.
+ * Classe de exemplo para o exercício da Aula 2 de Métodos.
  */
 public class Quadrilatero {
 
-    public static double area(double lado) {
+    public static void area(double lado) {
 
-        return lado * lado;
+        System.out.println("Área do quadrado:" + lado * lado);
     }
 
-    public static double area(double lado1, double lado2) {
+    public static void area(double lado1, double lado2) {
 
-        return lado1 * lado2;
+        System.out.println("Área do retângulo:" + lado1 * lado2);
     }
 
-    public static double area(double baseMaior, double baseMenor, double altura) {
+    public static void area(double baseMaior, double baseMenor, double altura) {
 
-        return ((baseMaior+baseMenor)*altura) / 2;
+        System.out.println("Área do trapézio:" + ((baseMaior+baseMenor)*altura) / 2);
     }
 
-    public static void xpto() {
+    public static void area(float diagonal1, float diagonal2) {
 
-        System.out.println("Antes");
-        return;
-    }
-
-    public static double abc() {
-        return 1.6;
+        System.out.println("Área do losango:" + (diagonal1 * diagonal2)/2);
     }
 
 }
