3a4eec987482239f538f497ee0f615416e24c023
A Driana <75712203+SilvaeAdriana@users.noreply.github.com>
2022-07-22 20:36:38 -0300

    Exercicio 3

--- Quadrilatero.java
+++ Quadrilatero.java
@@ -1,28 +1,33 @@
 package one.digitalinnovation.basecamp;
 
 /**
- * Classe de exemplo para o exercício da Aula 2 de Métodos.
+ * Classe de exemplo para o exercício da Aula 3 de Métodos.
  */
 public class Quadrilatero {
 
-    public static void area(double lado) {
+    public static double area(double lado) {
 
-        System.out.println("Área do quadrado:" + lado * lado);
+        return lado * lado;
     }
 
-    public static void area(double lado1, double lado2) {
+    public static double area(double lado1, double lado2) {
 
-        System.out.println("Área do retângulo:" + lado1 * lado2);
+        return lado1 * lado2;
     }
 
-    public static void area(double baseMaior, double baseMenor, double altura) {
+    public static double area(double baseMaior, double baseMenor, double altura) {
 
-        System.out.println("Área do trapézio:" + ((baseMaior+baseMenor)*altura) / 2);
+        return ((baseMaior+baseMenor)*altura) / 2;
     }
 
-    public static void area(float diagonal1, float diagonal2) {
+    public static void xpto() {
 
-        System.out.println("Área do losango:" + (diagonal1 * diagonal2)/2);
+        System.out.println("Antes");
+        return;
+    }
+
+    public static double abc() {
+        return 1.6;
     }
 
 }
